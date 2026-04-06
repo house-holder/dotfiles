bfd859eb32608ef3b726efcf4c963cb125e2fd13
kh <kylemhouseholder@gmail.com>
2026-04-06 15:08:50 -0500

    edited nvim options

--- omarchy-nvim/.config/nvim/lua/config/options.lua
+++ omarchy-nvim/.config/nvim/lua/config/options.lua
@@ -1,2 +1,47 @@
 -- Default opts: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+vim.schedule(function()
+  vim.opt.clipboard = "unnamedplus" -- :help 'clipboard'
+end)
+
 vim.opt.relativenumber = true
+vim.opt.list = true
+vim.opt.listchars = {
+  tab = "  ",
+  -- trail = '·',
+  trail = "￮",
+  -- tab = '·┈',
+  -- multispace = '￮',
+  -- lead = '  ',
+  extends = "▶",
+  precedes = "◀",
+  nbsp = "␣",
+}
+
+vim.opt.expandtab = false
+vim.opt.smarttab = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4 -- >> or << shifting
+vim.opt.softtabstop = 4
+vim.opt.breakindent = true -- line wrap to same indent level
+
+vim.opt.undofile = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.signcolumn = "yes"
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.mouse = "a" -- enabled for all
+vim.opt.showmode = false
+
+vim.opt.foldmethod = "expr"
+vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+vim.opt.foldlevel = 20
+vim.opt.updatetime = 250
+vim.opt.timeoutlen = 300
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+
+vim.opt.inccommand = "nosplit"
+vim.opt.cursorline = true
+vim.opt.scrolloff = 10
+vim.opt.confirm = false
